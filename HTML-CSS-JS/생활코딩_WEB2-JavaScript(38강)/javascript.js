a3ddb852ba91a940418dcbe8d0630cc78ecf91c7
Sangmin <ss721229@naver.com>
2021-07-30 22:29:25 +0900

    생활코딩 web2 완료

--- HTML-CSS-JS/생활코딩_WEB2-JavaScript(38강)/javascript.js
+++ HTML-CSS-JS/생활코딩_WEB2-JavaScript(38강)/javascript.js
@@ -1,19 +1,22 @@
 var Body = {
     setColor : function (color){
-        document.querySelector('body').style.color = color ;
+        $('body').css("color", color);
+        // document.querySelector('body').style.color = color ;
     },
     setBackgroundColor : function (color){
-        document.querySelector('body').style.backgroundColor = color ;
+        $('body').css("background-color", color);
+        // document.querySelector('body').style.backgroundColor = color ;
     }
 }
 function setColor(color){
     var spanlist = document.querySelectorAll('span');
-    var i = 0;
-    while(i < spanlist.length)
-        {
-            spanlist[i].style.color = color;
-            i+=1;
-        }
+    // var i = 0;
+    // while(i < spanlist.length)
+    //     {
+    //         spanlist[i].style.color = color;
+    //         i+=1;
+    //     }
+    $('span').css("color",color);
 }
 function allBtnChange(dayOrNight) {
     var btn = document.getElementsByClassName("handlerBtn");
